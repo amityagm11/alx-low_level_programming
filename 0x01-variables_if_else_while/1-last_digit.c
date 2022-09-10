53a8da87852e09dbfe0f505f896aa1c709665a90
amityagm11 <52486244+amityagm11@users.noreply.github.com>
2022-09-10 12:28:24 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 /**
  * main - main block
  * Description: Get a random number and check its last digit, compare it with 5
  * Return: 0
  */
+
 int main(void)
 {
 	int n;
