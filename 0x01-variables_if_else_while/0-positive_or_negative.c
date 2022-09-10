35d285836075bfaa453ae406cc776d7313361cbf
amityagm11 <52486244+amityagm11@users.noreply.github.com>
2022-09-10 12:12:59 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,9 @@
 
 /**
  *
- * *main - main function 
- * *Description: Get a random number and print the number, 
+ * main - main function 
+ * Description: Get a random number and print the number, 
+ *
  * it is positive, negative, or zero
  * Return: 0
  */
