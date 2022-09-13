74d23b5a8e167c0db0de0ff47ff02b0b4291ba25
amityagm11 <amityagm11@gmail.com>
2022-09-13 15:38:37 -0700

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,12 @@ int print_last_digit(int n)
 {
 	int last;
 
-	n = n >= 0 ? n : n * -1;
-	last = n % 10;
-	_putchar('0' + last);
+	if (n < 0)
+		last = -1 * (n % 10);
+	else
+		last = n % 10;
+
+	_putchar(last + '0');
 
 	return (last);
 }
