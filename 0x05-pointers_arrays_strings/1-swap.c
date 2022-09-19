f85f09fe22086dda68d44bceb17c04f41d97728b
amityagm11 <amityagm11@gmail.com>
2022-09-19 14:15:24 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap
+ * @a: pointer a
+ * @b: pointer b
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
