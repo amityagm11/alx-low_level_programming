11974d53d3ca2a9531b8bbd8a153b993abd55d90
amityagm11 <amityagm11@gmail.com>
2022-09-27 11:14:01 -0700

    07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - fills
+ * @s: pointer
+ * @b: constant
+ * @n: max bytes
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0 ; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
