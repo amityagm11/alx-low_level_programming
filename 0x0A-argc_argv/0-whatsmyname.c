e9eeb7beca2df1e8ae0c0e677289ba9c21fb5f69
amityagm11 <amityagm11@gmail.com>
2022-10-03 00:38:46 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: count argu
+ * @argv: list argument
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
