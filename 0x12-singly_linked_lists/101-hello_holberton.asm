e21735b6de62a7e42a7f89a47b61045f359a5e2b
amityagm11 <amityagm11@gmail.com>
2022-10-24 06:13:13 -0700

    hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,25 @@
+; File: 101-hello_holberton.asm
+; Auth: Amanuel Ayele G/Michael
+; Desc: 64-bit assembly program that prints hello
+
+
+extern printf
+section .text
+	global main
+
+main:
+	push rbp
+
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
+
+	pop rbp
+
+	mov rax,0
+	ret
+	
+	section .data
+		msg: db "Hello, Holberton", 0
+		fmt: db "%s", 10, 0
