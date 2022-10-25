251461bceb80897bde32c2c20c34871f6d078f1f
amityagm11 <amityagm11@gmail.com>
2022-10-24 23:55:00 -0700

    submit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node
+ * @head: head
+ * Return: null or head
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int ret;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (ret);
+}
